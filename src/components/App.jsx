429cc72af0d140100b8e4fbf10a58386cdd1185e
Abel-Zambrano <67209686+Abel-Zambrano@users.noreply.github.com>
2020-09-30 18:50:58 -0700

    added event listeners

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,9 +8,13 @@ function App() {
 
   const [isMouseOver, setMouseOver] = useState(false);
 
+  const [name, setName] = useState('');
+
+  const [nameText, setNameText] = useState('');
+
   function handleClicked() {
     setHeadingText('Submitted');
-    
+    // event.preventDefault(); // Prevents Form's next action
   }
 
   function handleMouseOver() {
@@ -21,17 +25,30 @@ function App() {
     setMouseOver(false);
   }
 
+  function handleChange(event) {
+    setName(event.target.value);
+    
+  }
+
+  function handleValue() {
+    setNameText(name);
+  }
+
   return (
     <div className="container">
       <h1>{headingText}</h1>
-      <input type="text" placeholder="What's your name?" />
+      <h1>{nameText}</h1>
+      {/* <form onSubmit={handleClicked}> */}
+      <input onChange={handleChange} type="text" placeholder="What's your name?" />
       <button 
         style={{backgroundColor: isMouseOver ? 'black' : 'white' }} 
-        onClick={handleClicked}
+        onClick={() => {handleClicked(); handleValue();}}
         onMouseOver={handleMouseOver}
         onMouseOut={handleMouseOut}
+        value={name}
       >Submit
       </button>
+      {/* </form> */}
     </div>
   );
 }
